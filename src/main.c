81e5786e7d586fd897446f492ce6a77dd07558f1
Ayush Singh <ayushsingh1325@gmail.com>
2023-06-05 14:23:17 +0530

    Initial commit
    Signed-off-by: Ayush Singh <ayushsingh1325@gmail.com>

---
+++ src/main.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+
+void main(void) {
+  for (;;) {
+    printk("Hello World!\n");
+    k_msleep(1000);
+  }
+}
