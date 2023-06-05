b7bcd7d13630d9c1b7d710545fbfb390c42a846a
Ayush Singh <ayushsingh1325@gmail.com>
2023-06-05 21:22:45 +0530

    Simple UART application
    An application to do something if anything is pushed to UART
    
    Signed-off-by: Ayush Singh <ayushsingh1325@gmail.com>

--- src/main.c
+++ src/main.c
@@ -4,11 +4,78 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <stdbool.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/init.h>
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(cc1352_greybus, CONFIG_BEAGLEPLAY_GREYBUS_LOG_LEVEL);
+
+#define UART_DEVICE_NODE DT_CHOSEN(zephyr_shell_uart)
+
+#define MSG_SIZE 32
+
+static const struct device *const uart_dev = DEVICE_DT_GET(UART_DEVICE_NODE);
+
+static const bool temp = true;
+
+K_MSGQ_DEFINE(uart_msgq, sizeof(bool), 10, 4);
+
+void serial_callback(const struct device *dev, void *user_data) {
+  uint8_t c;
+  bool flag = false;
+
+  if (!uart_irq_update(uart_dev)) {
+    return;
+  }
+
+  if (!uart_irq_rx_ready(uart_dev)) {
+    return;
+  }
+
+  while (uart_fifo_read(uart_dev, &c, 1) == 1) {
+    flag = true;
+  }
+
+  if (flag) {
+    k_msgq_put(&uart_msgq, &temp, K_NO_WAIT);
+  }
+}
+
+void print_uart(char *buf) {
+  int msg_len = strlen(buf);
+
+  for (int i = 0; i < msg_len; i++) {
+    uart_poll_out(uart_dev, buf[i]);
+  }
+}
 
 void main(void) {
-  for (;;) {
-    printk("Hello World!\n");
-    k_msleep(1000);
+  LOG_INF("Starting BeaglePlay Greybus");
+
+  bool tx;
+
+  if (!device_is_ready(uart_dev)) {
+    LOG_ERR("UART device not found!");
+    return;
+  }
+
+  int ret = uart_irq_callback_user_data_set(uart_dev, serial_callback, NULL);
+  if (ret < 0) {
+    if (ret == -ENOTSUP) {
+      LOG_ERR("Interrupt-driven UART API support not enabled\n");
+    } else if (ret == -ENOSYS) {
+      LOG_ERR("UART device does not support interrupt-driven API\n");
+    } else {
+      LOG_ERR("Error setting UART callback: %d\n", ret);
+    }
+    return;
+  }
+
+  uart_irq_rx_enable(uart_dev);
+
+  while (k_msgq_get(&uart_msgq, &tx, K_FOREVER) == 0) {
+    print_uart("Hello\r\n");
   }
 }
