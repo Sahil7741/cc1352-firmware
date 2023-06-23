8ea1229755a3efc5924329395d9164730944a8f6
Ayush Singh <ayushsingh1325@gmail.com>
2023-06-23 15:17:32 +0530

    Add better write function
    Signed-off-by: Ayush Singh <ayushsingh1325@gmail.com>

--- src/svc.c
+++ src/svc.c
@@ -5,6 +5,18 @@
 
 LOG_MODULE_DECLARE(cc1352_greybus, CONFIG_BEAGLEPLAY_GREYBUS_LOG_LEVEL);
 
+static int write_data(int sock, void *data, size_t len) {
+  int transmitted = 0;
+  while(transmitted < len) {
+    transmitted += zsock_send(sock, transmitted + (char*)data, len - transmitted, 0);
+    if(transmitted < 0) {
+      LOG_ERR("Failed to transmit data");
+      return -1;
+    }
+  }
+  return transmitted;
+}
+
 int svc_send_protocol_version_request(int sock) {
   int ret;
   struct gb_svc_version_request req;
@@ -18,13 +30,13 @@ int svc_send_protocol_version_request(int sock) {
   hdr.status = 0;
   hdr.size = sizeof(struct gb_svc_version_request) + sizeof(struct gb_operation_msg_hdr);
 
-  ret = zsock_send(sock, &hdr, sizeof(struct gb_operation_msg_hdr), 0);
+  ret = write_data(sock, &hdr, sizeof(struct gb_operation_msg_hdr));
   if(ret < 0) {
     LOG_ERR("Failed to send greybus header: %d", errno);
     return -1;
   }
 
-  ret = zsock_send(sock, &req, sizeof(struct gb_svc_version_request), 0);
+  ret = write_data(sock, &req, sizeof(struct gb_svc_version_request));
   if(ret < 0) {
     LOG_ERR("Failed to send greybus svc version data");
     return -1;
